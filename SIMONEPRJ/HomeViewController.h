be956b30f7b68e0d30e703d0ef98a0eef78386ff
laxminarasimhagude1995 <laxminarasimhagude@gmail.com>
2019-02-13 16:28:35 +0530

    Laxmi

---
+++ SIMONEPRJ/HomeViewController.h
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.h
+//  SIMONEPRJ
+//
+//  Created by Kwebmaker on 13/02/2019.
+//  Copyright © 2019 kwebmaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SIMONEPRJ/HomeViewController.m
@@ -0,0 +1,32 @@
+//
+//  HomeViewController.m
+//  SIMONEPRJ
+//
+//  Created by Kwebmaker on 13/02/2019.
+//  Copyright © 2019 kwebmaker. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
